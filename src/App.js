5b194e85223c116ca608504ec4519cc8bf32b469
priyam125 <sinha.priyam125>
2022-01-28 00:34:56 +0530

    Basic functioning structure done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
+import Board from './components/Board';
 
-function App() {
+const width = 8
+const candyColors = [
+    'blue',
+    'orange',
+    'purple',
+    'red',
+    'yellow',
+    'green'
+]
+
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex p-12 h-screen w-screen'>
+      <Board width={width} candyColors={candyColors} /> 
     </div>
   );
 }
